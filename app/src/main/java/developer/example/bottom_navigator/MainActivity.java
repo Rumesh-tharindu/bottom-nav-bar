1b67349b9b7436a9e5edc8aab958ed529f708b73
Rumesh-tharindu <rumeshtharindu96@gmail.com>
2020-09-03 22:26:08 +0530

    First commit

---
+++ app/src/main/java/developer/example/bottom_navigator/MainActivity.java
@@ -0,0 +1,14 @@
+package developer.example.bottom_navigator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
